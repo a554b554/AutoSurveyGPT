bd98293349d85bc0b9a7c8b3f97cd46205827bac
a554b554 <chang@cs.columbia.edu>
2023-05-17 22:13:48 -0700

    update pdf parser

--- gpt_config.py
+++ gpt_config.py
@@ -3,6 +3,7 @@ import os
 gen_query_model = "gpt-3.5-turbo"
 html_parse_model = "gpt-3.5-turbo"
 abstract_parse_model = "gpt-3.5-turbo"
+pdf_extraction_model = "gpt-4"
 
 def get_driver_path():
     if os.name == 'nt':
--- prompt.py
+++ prompt.py
@@ -40,3 +40,16 @@ def read_abstract_prompt(abstract, my_topic):
         "content": "Here is the abstract of a paper: [" + abstract + "]. Here is the description of my paper:[" + my_topic + "].  Please read them and answer my question: [Q1: What are the similarities between this paper and my idea? Q2: What are the difference between the paper and my idea? Q3: Please provide a similarity score from 1 to 5, where a higher score indicates greater relevance between two research papers. Use the following calibration for the scores: 1 - Not relevant: Papers from different fields with no shared methodologies or insights, e.g., one paper on natural language processing and the other on computer graphics. 2 - Somewhat relevant: Papers from the same subfield, such as adversarial learning, neural rendering, or tangible input interfaces. 3 - Relevant: Papers addressing similar problems (e.g., increasing the robustness of adversarial learning, tangible input interfaces in AR), or using similar methodologies to solve different problems. Papers with this level of similarity should be considered for citation. 4 - Very relevant: Papers addressing similar problems and using similar techniques. 5 - Mostly relevant: Papers addressing almost identical problems and using similar techniques.] Please provide your answer in .json format, the keys are <Similarity>, <Difference>, <Score>"
     }
     return [info_sys, info_user]
+
+
+def pdf_section_extraction_prompt(pdf_text):
+    info_sys = {
+        "role": "system",
+        "content": "You are a PDF parser, I want you to extract particular sections bases on my instructions."
+    }
+
+    info_user = {
+        "role": "user",
+        "content": "Here is the part text extracted from the pdf: [" + pdf_text + "]. I want you to parse me the Introduction section and Related work section. Please return your answer in .json format, with keys <Introduction> and <Related Work>. If either Introduction or Related work is not found, return empty string in corresponding json field. If found, extract full paragraph in both section. Do not use ... as I need the full paragraph for both introduction and related work."
+    }
+    return [info_sys, info_user]
